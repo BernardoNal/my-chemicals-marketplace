cc8c228ed472dec32808b274ae0bc5978b82a048
Bernardo Carvalho <nal-carvalho@hotmail.com>
2024-02-28 15:51:31 -0300

    model product

---
+++ db/migrate/20240228185026_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.float :price
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
