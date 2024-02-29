d2c09307c5ddb8df63136890d79a1b53975a907d
claraleal <clara.lidiax@gmail.com>
2024-02-29 13:55:20 -0300

    delete_product

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :products, only: %i[index show]
+  resources :products, only: %i[index show destroy]
 
-  # Defines the root path route ("/")
+  #delete "products/:id", to: "products#destroy"
+
+  #destroy"
+# Defines the root path route ("/")
   # root "posts#index"
 end
