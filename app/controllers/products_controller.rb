f34f0d593d5f1577310d5fca11e7207fde3f5981
henrique lopes <henrique.0.lopes@hotmail.com>
2024-02-29 10:53:47 -0300

    criado o metodo show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,12 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "/products", to: "products#index"
+  get "/products/:id", to: "products#show"
 
   # Defines the root path route ("/")
   # root "posts#index"
