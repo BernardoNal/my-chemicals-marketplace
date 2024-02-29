5e5b5dfd24acbb7984b57c74360e7f8f679bb7ad
jordano oliveira <jordano.oliveira@avalara.com>
2024-02-29 15:01:53 -0300

    Correção router

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-<<<<<<< HEAD
-  resources :products, only: %i[index show edit update]
-=======
   resources :products do
     get "myproducts", on: :collection
   end
->>>>>>> master
 
   #delete "products/:id", to: "products#destroy"
 
