c265d85e04f903516d0da5de2f8a73c9b06d775a
jordano oliveira <jordano.oliveira@avalara.com>
2024-02-29 14:53:45 -0300

    Inclusão dos métodos edit e update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,28 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index show]
-
+  skip_before_action :authenticate_user!, only: %i[index show edit update]
+  
   def index
     @products = Product.all
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.purchases.count.positive?
+      flash[:alert] = "Product has already been purchased and cannot be edited."
+    end
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
   end
 
   private
