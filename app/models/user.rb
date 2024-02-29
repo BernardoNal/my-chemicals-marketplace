382e17634000141a26608aede138bed814bf033b
henrique lopes <henrique.0.lopes@hotmail.com>
2024-02-29 08:47:00 -0300

    index basico completo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :address, presence: true
 
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :products
