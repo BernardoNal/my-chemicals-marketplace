1884b1f03246e5ed64196e4389483d40d3794fcb
Bernardo Carvalho <nal-carvalho@hotmail.com>
2024-03-04 10:03:40 -0300

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -68,7 +68,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_02_135101) do
     t.bigint "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.index ["product_id"], name: "index_reviews_on_product_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
